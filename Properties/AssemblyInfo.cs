72882a83dabf46a6f1df8af08b95d3a1409cb565
Robin van Poppel <github@nyaaa.nl>
2018-12-24 15:46:02 +0100

    Bump version to 0.9.7

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,9 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("KeeTrayTOTP")]
 [assembly: AssemblyDescription("Time based One Time Password Plugin to enable 2 step authentification by generating TOTPs from the KeePass tray icon. This plugin also provides a custom column to display and/or generate TOTP's. Auto-Type is also supported when the specified custom field is used in a keystroke sequence.")]
 [assembly: AssemblyConfiguration("")]
@@ -12,24 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Time Crash 2016")]
 [assembly: AssemblyTrademark("2016-01-29")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b4dcf5ee-e84b-40b2-88f6-15e948b15114")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.9.*")]
-[assembly: AssemblyFileVersion("0.9.6.0")]
+[assembly: AssemblyFileVersion("0.9.7.0")]
