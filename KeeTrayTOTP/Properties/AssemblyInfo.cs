909503530aa0bb645d696de140ac3e1d7897a0f5
Robin van Poppel <github@nyaaa.nl>
2019-11-01 11:50:25 +0100

    Bump version to 0.99

--- KeeTrayTOTP/Properties/AssemblyInfo.cs
+++ KeeTrayTOTP/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("KeeTrayTOTP")]
 [assembly: AssemblyDescription("Time based One Time Password Plugin to enable 2 step authentification by generating TOTPs from the KeePass tray icon. This plugin also provides a custom column to display and/or generate TOTP's. Auto-Type is also supported when the specified custom field is used in a keystroke sequence.")]
@@ -7,9 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Time Crash")]
 [assembly: AssemblyProduct("KeePass Plugin")]
 [assembly: AssemblyCopyright("Copyright © Time Crash 2019")]
-[assembly: AssemblyTrademark("2019-05-05")]
+[assembly: AssemblyTrademark("2019-11-01")]
 [assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("b4dcf5ee-e84b-40b2-88f6-15e948b15114")]
-[assembly: AssemblyVersion("0.98.*")]
-[assembly: AssemblyFileVersion("0.98.0.0")]
+[assembly: AssemblyVersion("0.99.*")]
+[assembly: AssemblyFileVersion("0.99.0.0")]
