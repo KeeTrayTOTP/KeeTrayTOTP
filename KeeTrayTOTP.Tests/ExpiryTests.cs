f6d12061c915610c0d73fbcaa2c74279d682258a
Robin van Poppel <github@nyaaa.nl>
2020-04-05 14:37:47 +0200

    Hide expired entries in the popup menu (#82)
    * Add an expired entry to Test.kdbx
    * Moved the filtering logic to a separate method
    * Added a few unit tests

---
+++ KeeTrayTOTP.Tests/ExpiryTests.cs
@@ -0,0 +1,46 @@
+﻿using KeePassLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace KeeTrayTOTP.Tests
+{
+    [TestClass]
+    public     class ExpiryTests
+    {
+        [TestMethod]
+        public void IsExpired_ShouldReturnTrue_WhenExpiresIsTrueAndExpiryTimeInThePast()
+        {
+            var pwEntry = new PwEntry(true, true)
+            {
+                Expires = true,
+                ExpiryTime = DateTime.MinValue
+            };
+
+            Assert.IsTrue(pwEntry.IsExpired());
+        }
+
+        [TestMethod]
+        public void IsExpired_ShouldReturnFalse_WhenExpiresIsTrueAndExpiryTimeInTheFuture()
+        {
+            var pwEntry = new PwEntry(true, true)
+            {
+                Expires = true,
+                ExpiryTime = DateTime.UtcNow.AddSeconds(2)
+            };
+
+            Assert.IsFalse(pwEntry.IsExpired());
+        }
+
+        [TestMethod]
+        public void IsExpired_ShouldReturnFalse_WhenExpiresIsFalseAndExpiryTimeInThePast()
+        {
+            var pwEntry = new PwEntry(true, true)
+            {
+                Expires = false,
+                ExpiryTime = DateTime.MinValue
+            };
+
+            Assert.IsFalse(pwEntry.IsExpired());
+        }
+    }
+}
