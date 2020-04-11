cfe912fd064290e2653a6be29818f419817f4578
Robin van Poppel <github@nyaaa.nl>
2020-04-11 17:03:17 +0200

    Remove unused methods

--- KeeTrayTOTP/Libraries/QRCoder/QRCodeGenerator.cs
+++ KeeTrayTOTP/Libraries/QRCoder/QRCodeGenerator.cs
@@ -569,46 +569,6 @@ namespace QRCoder
 
             private static class MaskPattern
             {
-                public static bool Pattern1(int x, int y)
-                {
-                    return (x + y) % 2 == 0;
-                }
-
-                public static bool Pattern2(int x, int y)
-                {
-                    return y % 2 == 0;
-                }
-
-                public static bool Pattern3(int x, int y)
-                {
-                    return x % 3 == 0;
-                }
-
-                public static bool Pattern4(int x, int y)
-                {
-                    return (x + y) % 3 == 0;
-                }
-
-                public static bool Pattern5(int x, int y)
-                {
-                    return ((int)(Math.Floor(y / 2d) + Math.Floor(x / 3d)) % 2) == 0;
-                }
-
-                public static bool Pattern6(int x, int y)
-                {
-                    return ((x * y) % 2) + ((x * y) % 3) == 0;
-                }
-
-                public static bool Pattern7(int x, int y)
-                {
-                    return (((x * y) % 2) + ((x * y) % 3)) % 2 == 0;
-                }
-
-                public static bool Pattern8(int x, int y)
-                {
-                    return (((x + y) % 2) + ((x * y) % 3)) % 2 == 0;
-                }
-
                 public static int Score(ref QRCodeData qrCode)
                 {
                     int score1 = 0,
