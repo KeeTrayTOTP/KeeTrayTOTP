466dd4a1463294d4db614cacb36a0d3760344844
Robin van Poppel <github@nyaaa.nl>
2020-04-11 16:44:56 +0200

    Cleanup usings

--- KeeTrayTOTP.Tests/Properties/AssemblyInfo.cs
+++ KeeTrayTOTP.Tests/Properties/AssemblyInfo.cs
@@ -1,4 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("7911feeb-d902-49d1-8a55-cd761d66d58d")]
