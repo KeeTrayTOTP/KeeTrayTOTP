65ff922eb9674be40c0e3e1fd7b381c36270d841
Robin van Poppel <github@nyaaa.nl>
2020-04-15 21:22:48 +0200

    Add reproduction

---
+++ KeeTrayTOTP.Tests/QRCodeGeneratorTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KeeTrayTOTP.Tests
+{
+    [TestClass]
+    public class QRCodeGeneratorTests
+    {
+        [TestMethod]
+        public void QRCodeGenerator_ShouldGenerateCodeWithValidKeyUrl()
+        {
+            var code = "otpauth://totp/Sample%20Entry%20%232:?secret=JBSWY3DPEHPK3PXP&issuer=Sample%20Entry%20%232";
+            using (var generator = new QRCoder.QRCodeGenerator())
+            {
+                var act = generator.CreateQrCode(code);
+
+                act.Should().NotBeNull();
+                act.Version.Should().Be(8);
+                act.ModuleMatrix.Should().HaveCount(57);
+            }
+        }
+    }
+}
